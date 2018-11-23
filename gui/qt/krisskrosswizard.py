80224641226ea2053831afb265d821189ffa59e3
djdt <10266332+djdt@users.noreply.github.com>
2018-11-23 14:22:59 +1100

    import works

--- gui/qt/krisskrosswizard.py
+++ gui/qt/krisskrosswizard.py
@@ -11,6 +11,8 @@ class KrissKrossWizard(QtWidgets.QWizard):
     def __init__(self,  config, parent=None):
         super().__init__(parent)
 
+        self.krisskrossdata = []
+
         self.addPage(KrissKrossStartPage())
         self.addPage(KrissKrossImportPage())
         self.addPage(KrissKrossConfigPage(config))
@@ -20,31 +22,36 @@ class KrissKrossWizard(QtWidgets.QWizard):
         self.resize(540, 480)
 
     def accept(self):
-        kkds = []
         config = self.field("config")
-        # Get config
+        layer_dict = {}
         if self.field("radio_numpy"):
-            layer_dict = {}
-            # Import each file
-            for path in self.field("paths"):
-                ld_group = importNpz(path)
+            # Use the config from the first file
+            ld_group = importNpz(self.field("paths")[0])
+            config = ld_group[0].config
+            for ld in ld_group:
+                layer_dict.setdefault(ld.isotope, []).append(ld)
+                # Import the rest of the files with overriden config
+            for path in self.field("paths")[1:]:
+                ld_group = importNpz(path, config)
                 for ld in ld_group:
                     layer_dict.setdefault(ld.isotope, []).append(ld)
-            # Transform for each isotope
-            for isotope, layers in layer_dict.items():
-                kkd = KrissKrossData(isotope=isotope, config=config,
-                                     source=self.fields["paths"][0])
-                kkd.fromLayers([layer.data for layer in layers])
-                kkds.append(kkd)
 
         elif self.field("radio_agilent"):
             for path in self.field("paths"):
-                # lds = importNpz(path, config)
-                pass
+                ld_group = importAgilentBatch(path, config)
+                for ld in ld_group:
+                    layer_dict.setdefault(ld.isotope, []).append(ld)
+
         elif self.field("radio_csv"):
             for path in self.field("paths"):
                 # lds = importNpz(path, config)
                 pass
+
+        for isotope, layers in layer_dict.items():
+            kkd = KrissKrossData(isotope=isotope, config=config,
+                                 source=self.field("paths")[0])
+            kkd.fromLayers([layer.data for layer in layers])
+            self.krisskrossdata.append(kkd)
 
         super().accept()
 
@@ -67,11 +74,11 @@ class KrissKrossStartPage(QtWidgets.QWizardPage):
         radio_csv = QtWidgets.QRadioButton("&CSV layers", self)
         radio_numpy.setChecked(True)
         # Todo redo csv imports
-        radio_csv.setCheckable(False)
+        radio_csv.setEnabled(False)
 
         self.registerField("radio_numpy", radio_numpy)
-        self.registerField("radio_agilent", radio_numpy)
-        self.registerField("radio_csv", radio_numpy)
+        self.registerField("radio_agilent", radio_agilent)
+        self.registerField("radio_csv", radio_csv)
 
         box_layout = QtWidgets.QVBoxLayout()
         box_layout.addWidget(radio_numpy)
@@ -103,20 +110,32 @@ class KrissKrossImportPage(QtWidgets.QWizardPage):
         box_layout.addWidget(self.list)
         dir_box.setLayout(box_layout)
         # Controls
+        check_horizontal = QtWidgets.QCheckBox("Horizontal first layer.")
+        check_horizontal.setChecked(True)
+        self.registerField("check_horizontal", check_horizontal)
+
+        # Buttons
         button_file = QtWidgets.QPushButton("Open")
         button_file.clicked.connect(self.buttonAdd)
         button_dir = QtWidgets.QPushButton("Open All...")
         button_dir.clicked.connect(self.buttonAddAll)
 
-        control_layout = QtWidgets.QVBoxLayout()
-        control_layout.addWidget(button_file)
-        control_layout.addWidget(button_dir)
-        control_widget = QtWidgets.QWidget(self)
-        control_widget.setLayout(control_layout)
+        button_layout = QtWidgets.QHBoxLayout()
+        button_layout.addWidget(button_file)
+        button_layout.addWidget(button_dir)
+        button_box = QtWidgets.QWidget()
+        button_box.setLayout(button_layout)
 
-        main_layout = QtWidgets.QHBoxLayout()
+        # control_layout = QtWidgets.QVBoxLayout()
+        # control_layout.addWidget(check_horizontal)
+        # control_layout.addWidget(button_box)
+        # control_widget = QtWidgets.QWidget(self)
+        # control_widget.setLayout(control_layout)
+
+        main_layout = QtWidgets.QVBoxLayout()
         main_layout.addWidget(dir_box)
-        main_layout.addWidget(control_widget)
+        main_layout.addWidget(check_horizontal)
+        main_layout.addWidget(button_box)
         self.setLayout(main_layout)
 
         self.registerField("paths", self, "paths")
@@ -132,6 +151,7 @@ class KrissKrossImportPage(QtWidgets.QWizardPage):
         elif self.field("radio_csv"):
             paths, _filter = QtWidgets.QFileDialog.getOpenFileNames(
                 self, "Select Files", "" "(*.csv)")
+
         for path in paths:
             self.list.addItem(path)
 
@@ -139,18 +159,25 @@ class KrissKrossImportPage(QtWidgets.QWizardPage):
         path = QtWidgets.QFileDialog.getExistingDirectory(
             self, "Select Directory", "",
             QtWidgets.QFileDialog.DontUseNativeDialog)
+        if len(path) == 0:
+            return
         files = os.listdir(path)
         files.sort()
 
-        if self.field("radio_numpy"):
-            ext = '.npz'
-        elif self.field("radio_agilent"):
+        ext = '.npz'
+        if self.field("radio_agilent"):
             ext = '.b'
         elif self.field("radio_csv"):
             ext = '.csv'
 
+        print(self.field("radio_numpy"))
+        print(self.field("radio_agilent"))
+        print(self.field("radio_csv"))
+        print(ext)
+
         for f in files:
             if f.lower().endswith(ext):
+                print(f)
                 self.list.addItem(os.path.join(path, f))
 
     def keyPressEvent(self, event):
@@ -159,6 +186,10 @@ class KrissKrossImportPage(QtWidgets.QWizardPage):
             for item in self.list.selectedItems():
                 self.list.takeItem(self.list.row(item))
         super().keyPressEvent(event)
+
+    def initializePage(self):
+        # No need for config if numpy are used
+        self.setFinalPage(self.field("radio_numpy"))
 
     def isComplete(self):
         min_files = 2
@@ -192,6 +223,7 @@ class KrissKrossConfigPage(QtWidgets.QWizardPage):
         for k in self.config.keys():
             v = float(getattr(self.form, k).text())
             self.config[k] = v
+        return True
 
     # @QtCore.pyqtProperty(dict)
     # def config(self):
