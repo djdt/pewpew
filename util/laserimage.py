b92d68217b719a70e3b1a383a1506c3b2b20c8ae
djdt <10266332+djdt@users.noreply.github.com>
2018-11-24 13:51:33 +1100

    finally fixed

--- util/laserimage.py
+++ util/laserimage.py
@@ -8,7 +8,7 @@ import numpy as np
 
 class LaserImage(object):
     def __init__(self, fig, ax, data, extent=None, aspect='auto',
-                 colorbar='right', colorbarlabel='',
+                 colorbar=None, colorbarpos='bottom', colorbarlabel='',
                  scalebar=True, label=None,
                  vmin='auto', vmax='auto', cmap='magma'):
 
@@ -20,13 +20,12 @@ class LaserImage(object):
         self.im = ax.imshow(data, cmap=cmap, interpolation='none',
                             vmin=vmin, vmax=vmax,
                             extent=extent, aspect=aspect)
+
         ax.set_axis_off()
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
         ax.set_facecolor('black')
-
-        if colorbar not in ['none', None]:
-            self.addColorBar(fig, ax, colorbar, colorbarlabel)
+        ax.axis('scaled')
 
         if scalebar:
             scalebar = ScaleBar(1.0, 'um', frameon=False, color='white')
@@ -39,24 +38,24 @@ class LaserImage(object):
                         ha='center',
                         color='white')
 
-    def addColorBar(self, fig, ax, pos, label):
-        div = make_axes_locatable(ax)
-        cax = div.append_axes(pos, size=0.1, pad=0.05)
-        if pos in ['right', 'left']:
-            orientation = 'vertical'
-        else:
-            orientation = 'horizontal'
-        fig.colorbar(self.im, label=label,
-                     cax=cax, orientation=orientation,
-                     ticks=MaxNLocator(nbins=6))
+        if colorbar is not None:
+            div = make_axes_locatable(ax)
+            cax = div.append_axes(colorbarpos, size=0.1, pad=0.05)
+            if colorbarpos in ['right', 'left']:
+                orientation = 'vertical'
+            else:
+                orientation = 'horizontal'
+            fig.colorbar(self.im, label=colorbarlabel,
+                         cax=cax, orientation=orientation,
+                         ticks=MaxNLocator(nbins=6))
 
-    def addScaleBar(self, ax):
-        scalebar = ScaleBar(1.0, 'um', frameon=False, color='white')
-        ax.add_artist(scalebar)
+#     def addScaleBar(self, ax):
+#         scalebar = ScaleBar(1.0, 'um', frameon=False, color='white')
+#         ax.add_artist(scalebar)
 
-    def addLabel(self, ax, label):
-        ax.annotate(isotopeFormat(label),
-                    xycoords='axes fraction', xy=(0.0, 1.0),
-                    textcoords='offset points', xytext=(16, -16),
-                    ha='center',
-                    color='white')
+#     def addLabel(self, ax, label):
+#         ax.annotate(isotopeFormat(label),
+#                     xycoords='axes fraction', xy=(0.0, 1.0),
+#                     textcoords='offset points', xytext=(16, -16),
+#                     ha='center',
+#                     color='white')
