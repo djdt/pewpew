d43251577f5da9bcca7a05461c7c3654fba8b746
djdt <10266332+djdt@users.noreply.github.com>
2018-11-26 12:13:48 +1100

    moved to a function

--- util/laserimage.py
+++ util/laserimage.py
@@ -6,20 +6,19 @@ from util.formatter import isotopeFormat
 import numpy as np
 
 
-class LaserImage(object):
-    def __init__(self, fig, ax, data, extent=None, aspect='auto',
-                 colorbar=None, colorbarpos='bottom', colorbarlabel='',
-                 scalebar=True, label=None,
-                 vmin='auto', vmax='auto', cmap='magma'):
+def plotLaserImage(fig, ax, data, extent=None, aspect='auto',
+                   colorbar=None, colorbarpos='bottom', colorbarlabel='',
+                   scalebar=True, label=None,
+                   vmin='auto', vmax='auto', cmap='magma'):
 
         if vmin is 'auto':
             vmin = np.percentile(data, 1)
         if vmax is 'auto':
             vmax = np.percentile(data, 99)
 
-        self.im = ax.imshow(data, cmap=cmap, interpolation='none',
-                            vmin=vmin, vmax=vmax,
-                            extent=extent, aspect=aspect)
+        im = ax.imshow(data, cmap=cmap, interpolation='none',
+                       vmin=vmin, vmax=vmax,
+                       extent=extent, aspect=aspect)
 
         ax.set_axis_off()
         ax.get_xaxis().set_visible(False)
@@ -45,9 +44,55 @@ class LaserImage(object):
                 orientation = 'vertical'
             else:
                 orientation = 'horizontal'
-            fig.colorbar(self.im, label=colorbarlabel,
+            fig.colorbar(im, label=colorbarlabel,
                          cax=cax, orientation=orientation,
                          ticks=MaxNLocator(nbins=6))
+
+        return im
+
+
+# class LaserImage(object):
+#     def __init__(self, fig, ax, data, extent=None, aspect='auto',
+#                  colorbar=None, colorbarpos='bottom', colorbarlabel='',
+#                  scalebar=True, label=None,
+#                  vmin='auto', vmax='auto', cmap='magma'):
+
+#         if vmin is 'auto':
+#             vmin = np.percentile(data, 1)
+#         if vmax is 'auto':
+#             vmax = np.percentile(data, 99)
+
+#         self.im = ax.imshow(data, cmap=cmap, interpolation='none',
+#                             vmin=vmin, vmax=vmax,
+#                             extent=extent, aspect=aspect)
+
+#         ax.set_axis_off()
+#         ax.get_xaxis().set_visible(False)
+#         ax.get_yaxis().set_visible(False)
+#         ax.set_facecolor('black')
+#         ax.axis('scaled')
+
+#         if scalebar:
+#             scalebar = ScaleBar(1.0, 'um', frameon=False, color='white')
+#             ax.add_artist(scalebar)
+
+#         if label is not None and label is not "":
+#             ax.annotate(isotopeFormat(label),
+#                         xycoords='axes fraction', xy=(0.0, 1.0),
+#                         textcoords='offset points', xytext=(16, -16),
+#                         ha='center',
+#                         color='white')
+
+#         if colorbar is not None:
+#             div = make_axes_locatable(ax)
+#             cax = div.append_axes(colorbarpos, size=0.1, pad=0.05)
+#             if colorbarpos in ['right', 'left']:
+#                 orientation = 'vertical'
+#             else:
+#                 orientation = 'horizontal'
+#             fig.colorbar(self.im, label=colorbarlabel,
+#                          cax=cax, orientation=orientation,
+#                          ticks=MaxNLocator(nbins=6))
 
 #     def addScaleBar(self, ax):
 #         scalebar = ScaleBar(1.0, 'um', frameon=False, color='white')
