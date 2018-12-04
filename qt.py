31b3dac9b48342e7248f93051aa4b5c1012435bd
djdt <10266332+djdt@users.noreply.github.com>
2018-12-04 16:47:15 +1100

    removed todos

--- qt.py
+++ qt.py
@@ -3,8 +3,6 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from gui.qt.mainwindow import MainWindow
 
-# TODO override close on mainwindow to cose images nicely
-# TODO get some good varied colormaps
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
