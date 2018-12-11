194716d2c5beddb5749e5b0dc15716542b78fc4a
djdt <10266332+djdt@users.noreply.github.com>
2018-12-12 09:02:23 +1100

    add name

--- util/importer.py
+++ util/importer.py
@@ -5,27 +5,32 @@ from util.laser import LaserData
 
 
 def importCsv(path, config_override=None, calibration=None):
-    with open(path, 'r') as fp:
+    with open(path, "r") as fp:
         line = fp.readline().strip()
-        isotope = fp.readline().strip().lstrip('#')
+        isotope = fp.readline().strip().lstrip("#")
         line = fp.readline().strip()
         if config_override is None:
             config = LaserData.DEFAULT_CONFIG
-            tokens = line.split(';')
+            tokens = line.split(";")
             for token in tokens:
-                k, v = token.split('=')
+                k, v = token.split("=")
                 config[k] = float(v)
         else:
             config = config_override
         # Remove any trim
-        config['trim'] = [0, 0]
+        config["trim"] = [0, 0]
 
-        data = np.genfromtxt(fp, delimiter=',', dtype=np.float64)
+        data = np.genfromtxt(fp, delimiter=",", dtype=np.float64)
 
     structured = np.empty(data.shape, dtype=[(isotope, np.float64)])
     structured[k] = data
     return LaserData(
-        data=structured, config=config, calibration=calibration, source=path)
+        data=structured,
+        config=config,
+        calibration=calibration,
+        name=os.path.splitext(os.path.basename(path))[1],
+        source=path,
+    )
 
 
 def importNpz(path, config_override=None, calibration_override=None):
@@ -40,16 +45,20 @@ def importNpz(path, config_override=None, calibration_override=None):
     lds = []
     npz = np.load(path)
 
-    for i, (type, config, calibration) in \
-            enumerate(zip(npz['_type'], npz['_config'], npz['_calibration'])):
+    for i, (type, config, calibration) in enumerate(
+        zip(npz["_type"], npz["_config"], npz["_calibration"])
+    ):
         lds.append(
             type(
-                data=npz[f'_data{i}'],
-                config=config_override
-                if config_override is not None else config,
+                data=npz[f"_data{i}"],
+                config=config_override if config_override is not None else config,
                 calibration=calibration_override
-                if calibration_override is not None else calibration,
-                source=path))
+                if calibration_override is not None
+                else calibration,
+                name=os.path.splitext(os.path.basename(path))[1],
+                source=path,
+            )
+        )
     return lds
 
 
@@ -65,16 +74,16 @@ def importAgilentBatch(path, config, calibration=None):
     data_files = []
     with os.scandir(path) as it:
         for entry in it:
-            if entry.name.lower().endswith('.d') and entry.is_dir():
-                file_name = entry.name[:entry.name.rfind('.')] + '.csv'
+            if entry.name.lower().endswith(".d") and entry.is_dir():
+                file_name = entry.name[: entry.name.rfind(".")] + ".csv"
                 data_files.append(os.path.join(entry.path, file_name))
     # Sort by name
     data_files.sort()
 
-    with open(data_files[0], 'r') as fp:
+    with open(data_files[0], "r") as fp:
         line = fp.readline()
         skip_header = 0
-        while line and not line.startswith('Time [Sec]'):
+        while line and not line.startswith("Time [Sec]"):
             line = fp.readline()
             skip_header += 1
 
@@ -82,21 +91,29 @@ def importAgilentBatch(path, config, calibration=None):
         if "Print" in fp.read().splitlines()[-1]:
             skip_footer = 1
 
-    cols = np.arange(1, line.count(',') + 1)
+    cols = np.arange(1, line.count(",") + 1)
 
     lines = [
         np.genfromtxt(
             f,
-            delimiter=',',
+            delimiter=",",
             names=True,
             usecols=cols,
             skip_header=skip_header,
             skip_footer=skip_footer,
-            dtype=np.float64) for f in data_files
+            dtype=np.float64,
+        )
+        for f in data_files
     ]
     data = np.vstack(lines)
 
-    return LaserData(data, config=config, calibration=calibration, source=path)
+    return LaserData(
+        data,
+        config=config,
+        calibration=calibration,
+        name=os.path.splitext(os.path.basename(path))[1],
+        source=path,
+    )
 
 
 def importThermoiCapLaser(path, config, calibration=None):
@@ -112,15 +129,15 @@ def importThermoiCapLaser(path, config, calibration=None):
     data_files = []
     with os.scandir(path) as it:
         for entry in it:
-            if entry.name.lower().endswith('.csv') and entry.is_file():
+            if entry.name.lower().endswith(".csv") and entry.is_file():
                 data_files.append(entry.path)
     # Sort by name
     data_files.sort()
 
-    with open(data_files[0], 'r') as fp:
+    with open(data_files[0], "r") as fp:
         line = fp.readline()
         skip_header = 0
-        while line and not line.startswith('Time'):
+        while line and not line.startswith("Time"):
             line = fp.readline()
             skip_header += 1
 
@@ -135,7 +152,9 @@ def importThermoiCapLaser(path, config, calibration=None):
             names=True,
             usecols=cols,
             skip_header=skip_header,
-            dtype=np.float64) for f in data_files
+            dtype=np.float64,
+        )
+        for f in data_files
     ]
     # We need to skip the first row as it contains junk
     data = np.vstack(lines)[1:]
@@ -153,7 +172,7 @@ def importThermoiCapCSV(path, config, calibration=None):
 
     returns LaserData"""
     data = {}
-    with open(path, 'r') as fp:
+    with open(path, "r") as fp:
         # Find delimiter
         line = fp.readline().strip()
         delimiter = line[-1]
@@ -165,10 +184,13 @@ def importThermoiCapCSV(path, config, calibration=None):
             _, _, isotope, data_type, line_data = line.split(delimiter, 4)
             if data_type == "Counter":
                 data.setdefault(isotope, []).append(
-                    np.genfromtxt([line_data],
-                                  delimiter=delimiter,
-                                  dtype=np.float64,
-                                  filling_values=0.0))
+                    np.genfromtxt(
+                        [line_data],
+                        delimiter=delimiter,
+                        dtype=np.float64,
+                        filling_values=0.0,
+                    )
+                )
             line = fp.readline()
 
     # Read the keys to ensure order is same
@@ -183,4 +205,9 @@ def importThermoiCapCSV(path, config, calibration=None):
     for k in keys:
         structured[k] = data[k]
     return LaserData(
-        structured, config=config, calibration=calibration, source=path)
+        structured,
+        config=config,
+        calibration=calibration,
+        name=os.path.splitext(os.path.basename(path)),
+        source=path,
+    )
