d23c0f8891981ac033c5511fced8846faf1c4792
djdt <10266332+djdt@users.noreply.github.com>
2018-12-13 13:39:41 +1100

    more widgets

--- gui/calibration.py
+++ gui/calibration.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+import numpy as np
 
 from matplotlib.lines import Line2D
 
@@ -11,14 +12,35 @@ class CalibrationWidget(QtWidgets.QDialog):
         super().__init__(parent)
 
         self.laser = laser
-        self.canvas = Canvas(self)
+        self.canvas = Canvas(parent=self)
+
+        self.combo_isotope = QtWidgets.QComboBox()
+        self.combo_isotope.addItems(self.laser.isotopes())
+        self.combo_isotope.currentIndexChanged.connect(self.onComboIsotope)
+
+        self.lineedit_levels = QtWidgets.QLineEdit()
+        self.lineedit_levels.setText("5")
+        self.lineedit_levels.setValidator(QtGui.QIntValidator(0, 20, 1))
+
+        self.table = QtWidgets.QTableWidget()
 
         layout_form = QtWidgets.QFormLayout()
+        layout_form.addWidget("Isotope:", self.combo_isotope)
+        layout_form.addWidget("Calibration Levels:", self.lineedit_levels)
+        layout_form.addWidget(self.table)
+
         layout_horz = QtWidgets.QHBoxLayout()
-        layout_main = QtWidgets.QVBoxLayout()
+        layout_horz.addLayout(layout_form)
+        layout_horz.addWidget(self.canvas)
+
         buttons = QtWidgets.QDialogButtonBox(
-            QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Close
+            QtWidgets.QDialogButtonBox.Apply | QtWidgets.QDialogButtonBox.Close
         )
+
+        layout_main = QtWidgets.QVBoxLayout()
+        layout_main.addLayout(layout_horz)
+        layout_main.addWidget(buttons)
+        self.setLayout(layout_main)
 
     def draw(self):
 
@@ -37,18 +59,24 @@ class CalibrationWidget(QtWidgets.QDialog):
             aspect=self.laser.aspect(),
             extent=self.laser.extent(trimmed=True),
         )
-
-        from matplotlib.lines import Line2D
-
-        for i in range(0, 1.0, 0.1):
+        for i in np.linspace(0, 1.0, 7):
             rect = Line2D(
                 (0.0, 1.0),
                 (i, i),
-                transform=self.ax.transAxes,
-                linewidth=5.0,
+                transform=self.canvas.ax.transAxes,
+                linewidth=2.0,
                 color="white",
             )
-            text = 
             self.canvas.ax.add_artist(rect)
+            self.canvas.ax.annotate(
+                f"{i}",
+                (0.0, i),
+                xytext=(10, 0),
+                textcoords='offset points',
+                xycoords="axes fraction",
+                horizontalalignment="left",
+                verticalalignment="center",
+                color="white",
+            )
 
         self.canvas.draw()
