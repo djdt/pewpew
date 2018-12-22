b1edc6564acdd273ced2b74f9984f4be3b7109a4
djdt <10266332+djdt@users.noreply.github.com>
2018-12-22 22:24:31 +1100

    fixed format error

--- util/formatter.py
+++ util/formatter.py
@@ -2,6 +2,6 @@ def formatIsotope(isotope, fstring="{mass}{element}"):
     element = isotope.strip("0123456789")
     mass = isotope.lower().strip("abcdefghijklmnopqrstuvwxyz")
     if element.isalpha() and mass.isdecimal():
-        return fstring.format(element.capitalize(), mass)
+        return fstring.format(element=element.capitalize(), mass=mass)
     else:
         return isotope
