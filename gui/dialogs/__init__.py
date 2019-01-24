0539c1fc0b0a5b837cbc3802fd02675f1532ec8a
djdt <10266332+djdt@users.noreply.github.com>
2019-01-24 16:57:46 +1100

    init is dynamic

--- gui/dialogs/__init__.py
+++ gui/dialogs/__init__.py
@@ -1,6 +1,16 @@
-from .applydialog import ApplyDialog
-from .calibration import CalibrationDialog
-from .config import ConfigDialog
-from .export import ExportDialog
-from .filtering import FilteringDialog
-from .trim import TrimDialog
+import inspect
+import os.path
+import pkgutil
+import sys
+from PyQt5.QtWidgets import QDialog
+from importlib import import_module
+
+for (_, name, _) in pkgutil.walk_packages([os.path.dirname(__file__)]):
+
+    imported_module = import_module('.' + name, package=__name__)
+
+    for i in dir(imported_module):
+        attribute = getattr(imported_module, i)
+
+        if inspect.isclass(attribute) and issubclass(attribute, QDialog):
+            setattr(sys.modules[__name__], i, attribute)
