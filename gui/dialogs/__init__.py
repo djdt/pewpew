7aca80c13b476f11fc3e7bcc5f2392a6cec3a3a9
djdt <10266332+djdt@users.noreply.github.com>
2019-01-24 16:08:32 +1100

    init for dialogs

---
+++ gui/dialogs/__init__.py
@@ -0,0 +1,6 @@
+from .applydialog import ApplyDialog
+from .calibration import CalibrationDialog
+from .config import ConfigDialog
+from .export import ExportDialog
+from .filtering import FilteringDialog
+from .trim import TrimDialog
