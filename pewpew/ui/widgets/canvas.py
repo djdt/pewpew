e97c42f3000d887f50824376f29dfce83cf87085
djdt <10266332+djdt@users.noreply.github.com>
2019-02-01 15:19:31 +1100

    zoom on drag

--- pewpew/ui/widgets/canvas.py
+++ pewpew/ui/widgets/canvas.py
@@ -25,6 +25,9 @@ class Canvas(FigureCanvasQTAgg):
         self.use_scalebar = True
         self.use_label = True
 
+        self.zoomed = False
+        self.zoom_rect = [(0.0, 0.0), (0.0, 0.0)]
+
         super().__init__(self.fig)
         self.setParent(parent)
         self.setStyleSheet("background-color:transparent;")
@@ -35,10 +38,10 @@ class Canvas(FigureCanvasQTAgg):
         if connect_mouse_events:
             self.mpl_connect("motion_notify_event", self.updateStatusBar)
             self.mpl_connect("axes_leave_event", self.clearStatusBar)
+            self.mpl_connect("button_press_event", self.onMousePress)
+            self.mpl_connect("button_release_event", self.onMouseRelease)
 
     def close(self) -> None:
-        self.mpl_disconnect("motion_notify_event")
-        self.mpl_disconnect("axes_leave_event")
         self.clearStatusBar()
         super().close()
 
@@ -65,7 +68,7 @@ class Canvas(FigureCanvasQTAgg):
             cmap=viewconfig["cmap"]["type"],
             colorbar=self.use_colorbar,
             colorbarpos="bottom",
-            colorbartext=laser.calibration[isotope]["unit"],
+            colorbartext=str(laser.calibration[isotope]["unit"]),
             extent=laser.extent(trimmed=True),
             fontsize=viewconfig["font"]["size"],
             interpolation=viewconfig["interpolation"].lower(),
@@ -81,6 +84,27 @@ class Canvas(FigureCanvasQTAgg):
     def clear(self) -> None:
         self.fig.clear()
         self.ax = self.fig.add_subplot(111)
+
+    def onMousePress(self, e: MouseEvent) -> None:
+        if e.inaxes == self.ax:
+            self.zoom_rect[0] = (e.xdata, e.ydata)
+
+    def onMouseRelease(self, e: MouseEvent) -> None:
+        if e.inaxes == self.ax:
+            self.zoomed = True
+            self.zoom_rect[1] = (e.xdata, e.ydata)
+            # Reset to full size
+            if self.zoom_rect[0] == self.zoom_rect[1]:
+                self.zoomed = False
+                extent = self.image.get_extent()
+                self.ax.set_xlim(extent[0], extent[1])
+                self.ax.set_ylim(extent[2], extent[3])
+            # Zoom to rect
+            else:
+                self.zoomed = True
+                self.ax.set_xlim(self.zoom_rect[0][0], self.zoom_rect[1][0])
+                self.ax.set_ylim(self.zoom_rect[0][1], self.zoom_rect[1][1])
+            self.draw()
 
     def updateStatusBar(self, e: MouseEvent) -> None:
         if e.inaxes == self.ax and self.window() is not None:
