330b8c747612ca52f9b9fff6d5c09bce5bb3a98f
djdt <10266332+djdt@users.noreply.github.com>
2019-02-03 18:08:29 +1100

    uneeded fig reference

--- pewpew/ui/widgets/canvas.py
+++ pewpew/ui/widgets/canvas.py
@@ -1,11 +1,10 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtWidgets
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 import numpy as np
 
 from pewpew.lib.formatter import formatIsotope
 from pewpew.lib.plotimage import plotLaserImage
-from pewpew.lib.plothelpers import coords2value
 
 from pewpew.lib.laser import LaserData
 from matplotlib.backend_bases import MouseEvent, LocationEvent
@@ -17,8 +16,9 @@ class Canvas(FigureCanvasQTAgg):
     def __init__(
         self, connect_mouse_events: bool = True, parent: QtWidgets.QWidget = None
     ) -> None:
-        self.fig = Figure(frameon=False, tight_layout=True, figsize=(5, 5), dpi=100)
-        self.ax = self.fig.add_subplot(111)
+        fig = Figure(frameon=False, tight_layout=True, figsize=(5, 5), dpi=100)
+        super().__init__(fig)
+        self.ax = self.figure.add_subplot(111)
         self.image = np.array([], dtype=np.float64)
 
         self.use_colorbar = True
@@ -26,9 +26,8 @@ class Canvas(FigureCanvasQTAgg):
         self.use_label = True
 
         self.zoomed = False
-        self.zoom_rect = [(0.0, 0.0), (0.0, 0.0)]
+        self.rubberband = QtWidgets.QRubberBand(QtWidgets.QRubberBand.Rectangle, self)
 
-        super().__init__(self.fig)
         self.setParent(parent)
         self.setStyleSheet("background-color:transparent;")
         self.setSizePolicy(
@@ -38,8 +37,10 @@ class Canvas(FigureCanvasQTAgg):
         if connect_mouse_events:
             self.mpl_connect("motion_notify_event", self.updateStatusBar)
             self.mpl_connect("axes_leave_event", self.clearStatusBar)
-            self.mpl_connect("button_press_event", self.onMousePress)
-            self.mpl_connect("button_release_event", self.onMouseRelease)
+
+            self.mpl_connect("motion_notify_event", self.zoomDragMotion)
+            self.mpl_connect("button_press_event", self.zoomDragBegin)
+            self.mpl_connect("button_release_event", self.zoomDragEnd)
 
     def close(self) -> None:
         self.clearStatusBar()
@@ -61,7 +62,7 @@ class Canvas(FigureCanvasQTAgg):
 
         # Plot the image
         self.image = plotLaserImage(
-            self.fig,
+            self.figure,
             self.ax,
             data,
             aspect=laser.aspect(),
@@ -82,13 +83,8 @@ class Canvas(FigureCanvasQTAgg):
         )
 
     def clear(self) -> None:
-        self.fig.clear()
-        self.ax = self.fig.add_subplot(111)
-
-    def onMousePress(self, e: MouseEvent) -> None:
-        if e.inaxes == self.ax:
-            self.zoomed = True
-            self.zoom_rect[0] = (e.xdata, e.ydata)
+        self.figure.clear()
+        self.ax = self.figure.add_subplot(111)
 
     def onMouseRelease(self, e: MouseEvent) -> None:
         if e.inaxes == self.ax:
@@ -106,18 +102,52 @@ class Canvas(FigureCanvasQTAgg):
                 self.ax.set_ylim(self.zoom_rect[0][1], self.zoom_rect[1][1])
             self.draw()
 
-    def mouseMoveEvent(self, event: QtGui.QMouseEvent) -> None:
-        print(event.pos())
-        print(self.mouseEventCoords(event.pos()))
-        print(self.ax.transData.inverted().transform_point((event.x(), event.y())))
-        pass
+    def zoomDragBegin(self, e: MouseEvent) -> None:
+        if e.inaxes and e.button == 1:  # left mouse
+            self.zoom_start = (e.xdata, e.ydata)
+            self.rubberband_origin = e.guiEvent.pos()
+            self.rubberband.setGeometry(
+                QtCore.QRect(self.rubberband_origin, QtCore.QSize())
+            )
+            self.rubberband.show()
+
+    def zoomDragMotion(self, e: MouseEvent) -> None:
+        if e.inaxes and e.button == 1:
+            if self.rubberband.isVisible():
+                self.rubberband.setGeometry(
+                    QtCore.QRect(self.rubberband_origin, e.guiEvent.pos()).normalized()
+                )
+
+    def zoomDragEnd(self, e: MouseEvent) -> None:
+        if e.inaxes:  # left mouse
+            self.zoom_end = (e.xdata, e.ydata)
+
+            if self.zoom_start == self.zoom_end:  # No movement
+                self.zoomed = False
+                extent = self.image.get_extent()
+                self.ax.set_xlim(extent[0], extent[1])
+                self.ax.set_ylim(extent[2], extent[3])
+            else:
+                self.zoomed = True
+                if self.zoom_start[0] < self.zoom_end[0]:
+                    self.ax.set_xlim(self.zoom_start[0], self.zoom_end[0])
+                else:
+                    self.ax.set_xlim(self.zoom_end[0], self.zoom_start[0])
+                if self.zoom_start[1] < self.zoom_end[1]:
+                    self.ax.set_ylim(self.zoom_start[1], self.zoom_end[1])
+                else:
+                    self.ax.set_ylim(self.zoom_end[1], self.zoom_start[1])
+
+            self.draw()
+
+        self.rubberband.hide()
 
     def updateStatusBar(self, e: MouseEvent) -> None:
-        pass
-        if e.inaxes == self.ax and self.window() is not None:
+        if e.inaxes == self.ax:
             x, y = e.xdata, e.ydata
-            v = coords2value(self.image, x, y)
-            self.window().statusBar().showMessage(f"{x:.2f},{y:.2f} [{v}]")
+            v = self.image.get_cursor_data(e)
+            if self.window() is not None and self.window().statusBar() is not None:
+                self.window().statusBar().showMessage(f"{x:.2f},{y:.2f} [{v}]")
 
     def clearStatusBar(self, e: LocationEvent = None) -> None:
         if self.window() is not None:
