6c308fd45dc88a87de94ac39c785cc5b522d17c0
djdt <10266332+djdt@users.noreply.github.com>
2019-03-01 14:46:04 +1100

    krisscross actual

--- pewpew/lib/krisskross.py
+++ pewpew/lib/krisskross.py
@@ -70,20 +70,6 @@ class KrissKrossData(LaserData):
             data = np.mean(data, axis=2)
         return data
 
-    def split(self) -> List[LaserData]:
-        lds = []
-        for data in np.dsplit(self.data, self.data.shape[2]):
-            # Strip the third dimension
-            lds.append(
-                KrissKrossData(
-                    data=data,
-                    config=self.config,
-                    calibration=self.calibration,
-                    source=self.source,
-                )
-            )
-        return lds
-
     def extent(self) -> Tuple[float, float, float, float]:
         # Image data is stored [rows][cols]
         extent = super().extent()
@@ -91,3 +77,41 @@ class KrissKrossData(LaserData):
 
     def layers(self) -> int:
         return self.data.shape[2]
+
+def krissKrossLayers2(
+    layers: List[np.ndarray], aspect: float, warmup: int, horizontal_first: bool = True
+) -> np.ndarray:
+
+    j = 0 if horizontal_first else 1
+    subpixel = 0.5, 0.5
+    sub_per_pixel = [int(1000.0 / np.gcm(1000, int(sp * 1000))) for sp in subpixel]
+    offset = subpixel[0] * sub_per_pixel[0], subpixel[1] * sub_per_pixel[1]
+
+    aspect = int(aspect)
+    trim = int(aspect / 2)
+    # Calculate the line lengths
+    length = (layers[1].shape[0] * aspect, layers[0].shape[0] * aspect)
+
+    # Reshape the layers and stack into matrix
+    transformed = []
+    for i, layer in enumerate(layers):
+        # Trim data of warmup time and excess
+        layer = layer[:, warmup : warmup + length[(i + j) % 2]]
+        # Stretch array
+        layer = np.repeat(layer, aspect, axis=0)  # Here we would repeat by the overlap
+        # Flip vertical layers and trim
+        if (i + j) % 2 == 1:
+            layer = layer.T
+            layer = layer[trim:, trim:]
+        elif trim > 0:
+            layer = layer[:-trim, :-trim]
+
+        transformed.append(layer)
+
+    data = np.dstack(transformed)
+
+    return data
+
+
+if __name__ == "__main__":
+    pass
