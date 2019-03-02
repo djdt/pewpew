c33ee450192919a06d28f302600b157ba1ccedf9
djdt <10266332+djdt@users.noreply.github.com>
2019-03-03 08:49:40 +1100

    krisskross redo

--- pewpew/__main__.py
+++ pewpew/__main__.py
@@ -4,11 +4,65 @@ from PyQt5.QtWidgets import QApplication
 from pewpew.ui import MainWindow
 
 
+import numpy as np
+from typing import List, Tuple
+from pewpew.lib.laser import LaserData
+from pewpew.lib.krisskross import krissKrossLayers
+
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    config = dict(LaserData.DEFAULT_CONFIG)
+    config["spotsize"] = 10.0
+    config["speed"] = 10.0
+    config["scantime"] = 0.1
 
-    window = MainWindow()
-    sys.excepthook = window.exceptHook  # type: ignore
-    window.show()
+    horz = LaserData(
+        np.array(
+            [
+                [(1), (0), (1), (0), (1)],
+                [(0), (1), (0), (1), (0)],
+                [(1), (0), (1), (0), (1)],
+                [(0), (1), (0), (1), (0)],
+                [(1), (0), (1), (0), (1)],
+            ],
+            dtype=[("a", np.float64)],
+        ),
+        config,
+    )
+    vert = LaserData(
+        np.array(
+            [
+                [(0), (0), (0), (0), (0)],
+                [(0), (1), (1), (1), (0)],
+                [(0), (1), (0), (1), (0)],
+                [(0), (1), (1), (1), (0)],
+                [(0), (0), (0), (0), (0)],
+            ],
+            dtype=[("a", np.float64)],
+        ),
+        config,
+    )
+    # from pewpew.lib.io import agilent
+    # horz = agilent.load("/home/tom/Downloads/raw/Horz.b", config)
+    # vert = agilent.load("/home/tom/Downloads/raw/Vert.b", config)
 
-    app.exec()
+    data = krissKrossLayers(
+        [l.data for l in [horz, vert]],
+        1, 0
+    )
+
+    import matplotlib.pyplot as plt
+
+    plt.imshow(data["a"].mean(axis=2))
+
+    plt.show()
+
+
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)[]
+
+#     window = MainWindow()
+#     sys.excepthook = window.exceptHook  # type: ignore
+#     window.show()
+
+#     app.exec()
