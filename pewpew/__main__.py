60e17be9b90a2de569a662ab4e9f09ee732a377f
djdt <10266332+djdt@users.noreply.github.com>
2019-03-19 18:09:57 +1100

    import icons

--- pewpew/__main__.py
+++ pewpew/__main__.py
@@ -6,8 +6,8 @@ from pewpew.ui import MainWindow
 
 if __name__ == "__main__":
     if sys.platform in ['win32', 'darwin']:
-        from PyQt5.QtGui import QIcon
-        QIcon.setThemeName('breath')
+        from pewpew import icons
+        icons.qInitResources()  # type: ignore
 
     app = QApplication(sys.argv)
 
