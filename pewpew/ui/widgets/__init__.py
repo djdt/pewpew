684a2b99afddcd5883adc408280be48d936ae6e9
djdt <10266332+djdt@users.noreply.github.com>
2019-05-07 08:46:56 +1000

    add dragselector

--- pewpew/ui/widgets/__init__.py
+++ pewpew/ui/widgets/__init__.py
@@ -1,5 +1,6 @@
 from .basictable import BasicTable
 from .canvas import Canvas
 from .detailederror import DetailedError
+from .dragselector import DragSelector
 from .multipledirdialog import MultipleDirDialog
 from .overwritefileprompt import OverwriteFilePrompt
