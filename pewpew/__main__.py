95b252bc034ac3143b48ba0d2222731e6430d7dd
djdt <10266332+djdt@users.noreply.github.com>
2019-06-05 12:00:46 +1000

    add flake8 ignores and timer to keep event loop active

--- pewpew/__main__.py
+++ pewpew/__main__.py
@@ -1,12 +1,14 @@
 import sys
 
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
-from PyQt5 import QtGui
 from pewpew.ui import MainWindow
-from pewpew.resource import app_icon
+from pewpew.resource import app_icon  # noqa: F401
+
 if sys.platform in ['win32', 'darwin']:
-    from pewpew.resource import breath_icons
-    QtGui.QIcon.setThemeName("breath")
+    from pewpew.resource import breath_icons  # noqa: F401
+    QIcon.setThemeName("breath")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -14,6 +16,11 @@ if __name__ == "__main__":
     window = MainWindow()
     sys.excepthook = window.exceptHook  # type: ignore
     window.show()
-    window.setWindowIcon(QtGui.QIcon(":/app.ico"))
+    window.setWindowIcon(QIcon(":/app.ico"))
+
+    # Keep event loop active with timer
+    timer = QTimer()
+    timer.timeout.connect(lambda: None)
+    timer.start(100)
 
     app.exec()
