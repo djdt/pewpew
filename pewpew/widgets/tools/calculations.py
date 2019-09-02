6fbf28f987cf3429378ef908327a241d64399f22
djdt <10266332+djdt@users.noreply.github.com>
2019-09-02 15:25:16 +1000

    mostly there

--- pewpew/widgets/tools/calculations.py
+++ pewpew/widgets/tools/calculations.py
@@ -1,13 +1,23 @@
+import numpy as np
+
 from PySide2 import QtCore, QtGui, QtWidgets
 
-from pewpew.lib.calc import FormulaException, FormulaParser
+from pewpew.lib.calc import FormulaParser
+from pewpew.lib.viewoptions import ViewOptions
+
+from pewpew.widgets.canvases import LaserCanvas
+from pewpew.widgets.docks import LaserImageDock
+from pewpew.widgets.tools import Tool
+
+from typing import Union
+
 
 class FormulaLineEdit(QtWidgets.QLineEdit):
     def __init__(self, text: str, variables: dict, parent: QtWidgets.QWidget = None):
         super().__init__(text, parent)
 
         self.parser = FormulaParser(variables)
-        self.textEdited.connect(self.updateParser)
+        self.textChanged.connect(self.updateParser)
 
         self._valid = True
 
@@ -30,3 +40,86 @@ class FormulaLineEdit(QtWidgets.QLineEdit):
 
     def hasAcceptableInput(self) -> bool:
         return self.valid
+
+    def value(self) -> Union[float, np.ndarray]:
+        return self.parser.reduceString(self.text())
+
+
+class CalculationsTool(Tool):
+    def __init__(
+        self,
+        dock: LaserImageDock,
+        viewoptions: ViewOptions,
+        parent: QtWidgets.QWidget = None,
+    ):
+        super().__init__(parent)
+        self.dock = dock
+        # Custom viewoptions
+        self.viewoptions = ViewOptions()
+        self.viewoptions.canvas.colorbar = False
+        self.viewoptions.canvas.label = False
+        self.viewoptions.canvas.scalebar = False
+        self.viewoptions.image.cmap = viewoptions.image.cmap
+
+        variables = {k: v.data for k, v in self.dock.laser.data.items()}
+        self.formula = FormulaLineEdit(
+            str(self.dock.laser.isotopes[0]), variables=variables
+        )
+        self.formula.textChanged.connect(self.updateCanvas)
+        self.canvas = LaserCanvas(self.viewoptions)
+
+        self.combo_isotopes = QtWidgets.QComboBox()
+        self.combo_isotopes.addItem("Isotopes")
+        self.combo_isotopes.addItems(self.dock.laser.isotopes)
+        self.combo_isotopes.activated.connect(self.insertVariable)
+
+        layout_form = QtWidgets.QFormLayout()
+        layout_form.addRow("Insert:", self.combo_isotopes)
+        layout_form.addRow("Formula:", self.formula)
+
+        self.layout_center.addWidget(self.canvas)
+        self.layout_center.addLayout(layout_form)
+
+        self.updateCanvas()
+
+    def insertVariable(self, index: int) -> None:
+        if index == 0:
+            return
+        self.formula.setText(self.formula.text() + " " + self.combo_isotopes.currentText())
+        self.combo_isotopes.setCurrentIndex(0)
+
+    def updateCanvas(self) -> None:
+        if not self.formula.valid:
+            return
+        result = self.formula.value()
+        # Remove all nan and inf values
+        result = np.where(np.isfinite(result), result, 0.0)
+        if isinstance(result, np.ndarray):
+            extent = self.dock.laser.config.data_extent(result)
+            self.canvas.drawData(result, extent)
+            self.canvas.draw()
+
+    @QtCore.Slot("QWidget*")
+    def mouseSelectFinished(self, widget: QtWidgets.QWidget) -> None:
+        if widget is not None and hasattr(widget, "laser"):
+            self.dock = widget
+            self.formula.clear()
+            self.formula.valid = True
+            self.formula.variables = self.dock.laser.data
+
+        self.dockarea.mouseSelectFinished.disconnect(self.mouseSelectFinished)
+        self.activateWindow()
+        self.setFocus(QtCore.Qt.OtherFocusReason)
+        self.show()
+        self.draw()
+
+    def keyPressEvent(self, event: QtCore.QEvent) -> None:
+        if event.key() in [
+            QtCore.Qt.Key_Escape,
+            QtCore.Qt.Key_Enter,
+            QtCore.Qt.Key_Return,
+        ]:
+            return
+        if event.key() == QtCore.Qt.Key_F5:
+            self.draw()
+        super().keyPressEvent(event)
