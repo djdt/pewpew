617a53a51825cef744b28e5fbddf32c21db37808
djdt <10266332+djdt@users.noreply.github.com>
2019-09-02 15:30:27 +1000

    cleanup

--- pewpew/widgets/tools/calculations.py
+++ pewpew/widgets/tools/calculations.py
@@ -61,16 +61,11 @@ class CalculationsTool(Tool):
         self.viewoptions.canvas.scalebar = False
         self.viewoptions.image.cmap = viewoptions.image.cmap
 
-        variables = {k: v.data for k, v in self.dock.laser.data.items()}
-        self.formula = FormulaLineEdit(
-            str(self.dock.laser.isotopes[0]), variables=variables
-        )
+        self.formula = FormulaLineEdit("", variables={})
         self.formula.textChanged.connect(self.updateCanvas)
         self.canvas = LaserCanvas(self.viewoptions)
 
         self.combo_isotopes = QtWidgets.QComboBox()
-        self.combo_isotopes.addItem("Isotopes")
-        self.combo_isotopes.addItems(self.dock.laser.isotopes)
         self.combo_isotopes.activated.connect(self.insertVariable)
 
         layout_form = QtWidgets.QFormLayout()
@@ -80,12 +75,14 @@ class CalculationsTool(Tool):
         self.layout_center.addWidget(self.canvas)
         self.layout_center.addLayout(layout_form)
 
-        self.updateCanvas()
+        self.newDockAdded()
 
     def insertVariable(self, index: int) -> None:
         if index == 0:
             return
-        self.formula.setText(self.formula.text() + " " + self.combo_isotopes.currentText())
+        self.formula.setText(
+            self.formula.text() + " " + self.combo_isotopes.currentText()
+        )
         self.combo_isotopes.setCurrentIndex(0)
 
     def updateCanvas(self) -> None:
@@ -99,13 +96,19 @@ class CalculationsTool(Tool):
             self.canvas.drawData(result, extent)
             self.canvas.draw()
 
+    def newDockAdded(self) -> None:
+        self.combo_isotopes.clear()
+        self.combo_isotopes.addItem("Isotopes")
+        self.combo_isotopes.addItems(self.dock.laser.isotopes)
+
+        self.formula.parser.variables = {k: v.data for k, v in self.dock.laser.data.items()}
+        self.formula.valid = True
+        self.formula.setText(self.dock.laser.isotopes[0])
+
     @QtCore.Slot("QWidget*")
     def mouseSelectFinished(self, widget: QtWidgets.QWidget) -> None:
         if widget is not None and hasattr(widget, "laser"):
             self.dock = widget
-            self.formula.clear()
-            self.formula.valid = True
-            self.formula.variables = self.dock.laser.data
 
         self.dockarea.mouseSelectFinished.disconnect(self.mouseSelectFinished)
         self.activateWindow()
