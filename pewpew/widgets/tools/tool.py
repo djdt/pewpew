3786af28d1b2cbd71299db7c6c0a8c2f02e20d97
djdt <10266332+djdt@users.noreply.github.com>
2019-09-05 17:25:59 +1000

    mouseselect funcs / button

--- pewpew/widgets/tools/tool.py
+++ pewpew/widgets/tools/tool.py
@@ -4,28 +4,31 @@ from pewpew.widgets.dialogs import ApplyDialog
 
 
 class Tool(ApplyDialog):
-    mouseSelectFinished = QtCore.Signal()
+    mouseSelectStarted = QtCore.Signal("QWidget*")
 
     def __init__(self, parent: QtWidgets.QWidget = None):
         super().__init__(parent)
         self.setSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.MinimumExpanding)
+
+        self.button_select = QtWidgets.QPushButton("Select &Image")
+        self.button_select.pressed.connect(self.startMouseSelect)
+
         self.layout_top = QtWidgets.QHBoxLayout()
+        self.layout_top.addWidget(self.button_select, 0, QtCore.Qt.AlignRight)
         self.layout().insertLayout(0, self.layout_top)
 
     def sizeHint(self) -> QtCore.QSize:
         return QtCore.QSize(800, 600)
 
-    # @QtCore.Slot("QWidget*")
-    # def mouseSelectFinished(self, widget: QtWidgets.QWidget) -> None:
-    #     self.mouseSelectFinished.emit()
-    #     if widget is not None and hasattr(widget, "laser"):
-    #         self.dock = widget
+    def startMouseSelect(self) -> None:
+        self.hide()
+        self.mouseSelectStarted.emit(self)
 
-    #     self.dockarea.mouseSelectFinished.disconnect(self.mouseSelectFinished)
-    #     self.activateWindow()
-    #     self.setFocus(QtCore.Qt.OtherFocusReason)
-    #     self.show()
-    #     self.draw()
+    @QtCore.Slot("QWidget*")
+    def endMouseSelect(self, widget: QtWidgets.QWidget) -> None:
+        self.activateWindow()
+        self.setFocus(QtCore.Qt.OtherFocusReason)
+        self.show()
 
     def refresh(self) -> None:
         pass
