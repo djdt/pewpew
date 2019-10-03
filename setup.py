2aaf1f3e9fa1d0e1d337de26f09ea51dc365a853
djdt <10266332+djdt@users.noreply.github.com>
2019-10-03 22:14:00 +1000

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+from pewpew import __version__
+
+setup(
+    name="pewpew",
+    description="GUI for visualisation and manipulation of LA-ICP-MS data.",
+    author="djdt",
+    version=__version__,
+    packages=find_packages(include=["pewpew", "pewpew.*"]),
+    install_requires=["numpy", "matplotlib>=3.0.0"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "pytest-qt"],
+)
