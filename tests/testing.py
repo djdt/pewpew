6ef3b843041562c717889a8a9c9e09d1ff03b336
djdt <10266332+djdt@users.noreply.github.com>
2019-10-07 17:22:07 +1100

    generator for more organised data

--- tests/testing.py
+++ tests/testing.py
@@ -3,6 +3,17 @@ import numpy as np
 from typing import List, Union
 
 
+def linear_data(names: Union[str, List[str]]) -> np.ndarray:
+    names = names if isinstance(names, list) else [names]
+    dtype = [(name, float) for name in names]
+    data = np.empty((10, 10), dtype=dtype)
+    for name in names:
+        data[name] = np.repeat(
+            np.arange(0, 10, dtype=float).reshape((10, 1)), 10, axis=1
+        )
+    return data
+
+
 def rand_data(names: Union[str, List[str]]) -> np.ndarray:
     names = names if isinstance(names, list) else [names]
     dtype = [(name, float) for name in names]
