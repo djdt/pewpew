65b3caa4879b68bc10030f12c2d66a6a997ee0f2
djdt <10266332+djdt@users.noreply.github.com>
2020-04-10 17:44:43 +1000

    remove unused code

--- pewpew/lib/calc.py
+++ pewpew/lib/calc.py
@@ -47,61 +47,6 @@ def otsu(x: np.ndarray) -> float:
     return bin_centers[i]
 
 
-# def rolling_mean_filter(
-#     x: np.ndarray, window: Tuple[int, int], threshold: int = 3
-# ) -> np.ndarray:
-#     """Rolling mean filter an array.
-
-#     The window size should be an integer divisor of the array size.
-
-#     Args:
-#         window: Shape of the rolling window.
-#         threshold: σ's value must be from mean to be an outlier.
-
-#     """
-#     x = x.copy()
-#     # Create view
-#     roll = rolling_window(x, window)
-#     # Distance from mean (in stdevs)
-#     means = np.mean(roll, axis=(2, 3), keepdims=True)
-#     stds = np.std(roll, axis=(2, 3), keepdims=True)
-#     diffs = np.abs(roll - means) / stds
-#     # Recalculate mean, without outliers
-#     roll[diffs > threshold] = np.nan
-#     means = np.nanmean(roll, axis=(2, 3), keepdims=True)
-#     # Replace all outliers and copy back into view
-#     np.copyto(roll, means, where=diffs > threshold)
-#     return x
-
-
-# def rolling_median_filter(
-#     x: np.ndarray, window: Tuple[int, int], threshold: int = 3
-# ) -> np.ndarray:
-#     """Rolling median filter an array.
-
-#     The window size should be an integer divisor of the array size.
-
-#     Args:
-#         window: Shape of the rolling window.
-#         threshold: N-distance's from median to be considered outlier.
-
-#     """
-#     x = x.copy()
-#     # Create view
-#     roll = rolling_window(x, window)
-#     # Distance from the median
-#     medians = np.median(roll, axis=(2, 3), keepdims=True)
-#     diffs = np.abs(roll - medians)
-#     # Median difference
-#     median_diffs = np.median(diffs, axis=(2, 3), keepdims=True)
-#     # Normalise differences
-#     diffs = np.divide(diffs, median_diffs, where=median_diffs != 0)
-#     # Replace all over threshold and copy back into view
-#     np.copyto(roll, medians, where=diffs > threshold)
-
-#     return x
-
-
 def view_as_blocks(x: np.ndarray, block: Tuple[int, int]) -> np.ndarray:
     """Create non-overlapping views into a array.
     https://github.com/scikit-image/scikit-image/blob/master/skimage/util/shape.py
@@ -156,26 +101,3 @@ def shuffle_blocks(
 
     # Reform the image and then trim off excess
     return np.hstack(np.hstack(blocks.reshape(shape)))[: x.shape[0], : x.shape[1]]
-
-
-# def rolling_window_step(
-#     x: np.ndarray, window: Tuple[int, int], step: int
-# ) -> np.ndarray:
-#     """Create overlapping views into a array.
-
-#     Args:
-#         x: The array.
-#         window: The size of the view.
-#         step: Offset of the next window.
-
-#     Returns:
-#         An array of views.
-#     """
-#     x = np.ascontiguousarray(x)
-#     slices = tuple(slice(None, None, st) for st in (step,) * x.ndim)
-#     shape = tuple(
-#         list(((np.array(x.shape) - np.array(window)) // np.array(step)) + 1)
-#         + list(window)
-#     )
-#     strides = tuple(list(x[slices].strides) + list(x.strides))
-#     return np.lib.stride_tricks.as_strided(x, shape=shape, strides=strides)
