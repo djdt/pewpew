d4660853c2eaae5a1a99f77866b0842c78811a17
djdt <10266332+djdt@users.noreply.github.com>
2020-06-05 11:42:32 +1000

    move main into __main__

--- pewpew/__init__.py
+++ pewpew/__init__.py
@@ -1,29 +1,10 @@
 import sys
-from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2 import QtCore, QtWidgets
 
 __version__ = "1.0.1"
 
-from pewpew.mainwindow import MainWindow
-from pewpew.resources import app_icon  # noqa: F401
-from pewpew.resources import icons  # noqa: F401
-
-
-def main() -> None:
-    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
-    if sys.platform != "win32":
-        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_DontUseNativeDialogs)
-    QtGui.QIcon.setThemeName("breath")
-
-    app = QtWidgets.QApplication(sys.argv)
-
-    window = MainWindow()
-    sys.excepthook = window.exceptHook
-    window.show()
-    window.setWindowIcon(QtGui.QIcon(":/app.ico"))
-
-    # Keep event loop active with timer
-    timer = QtCore.QTimer()
-    timer.timeout.connect(lambda: None)
-    timer.start(100)
-
-    app.exec_()
+# Set Some Qt attributes
+QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+# Until KDE fix directory viewer
+if sys.platform != "win32":
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_DontUseNativeDialogs)
--- pewpew/__main__.py
+++ pewpew/__main__.py
@@ -1,4 +1,28 @@
-from pewpew import main
+import sys
+from PySide2 import QtCore, QtGui, QtWidgets
+
+from pewpew.mainwindow import MainWindow
+from pewpew.resources import app_icon  # noqa: F401
+from pewpew.resources import icons  # noqa: F401
+
+
+QtGui.QIcon.setThemeName("breath")
+
+
+def main() -> None:
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = MainWindow()
+    sys.excepthook = window.exceptHook
+    window.show()
+    window.setWindowIcon(QtGui.QIcon(":/app.ico"))
+
+    # Keep event loop active with timer
+    timer = QtCore.QTimer()
+    timer.timeout.connect(lambda: None)
+    timer.start(100)
+
+    app.exec_()
 
 
 if __name__ == "__main__":
