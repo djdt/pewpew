0dea6e4464cb4dd01b7186e1832e776c688dcd9b
djdt <10266332+djdt@users.noreply.github.com>
2020-07-03 11:54:30 +1000

    tool base now handles all buttons and signals

--- pewpew/widgets/tools/tool.py
+++ pewpew/widgets/tools/tool.py
@@ -4,28 +4,79 @@ from pewpew.widgets.views import _ViewWidget
 
 
 class ToolWidget(_ViewWidget):
-    def __init__(self, widget: _ViewWidget):
+    applyPressed = QtCore.Signal()
+    applyAllPressed = QtCore.Signal()
+
+    def __init__(self, widget: _ViewWidget, apply_all: bool = False):
         super().__init__(widget.view, editable=False)
         self.widget = widget
 
         self.layout_main = QtWidgets.QVBoxLayout()
 
-        self.layout_buttons = QtWidgets.QHBoxLayout()
+        self.button_box = QtWidgets.QDialogButtonBox(
+            QtWidgets.QDialogButtonBox.Cancel
+            | QtWidgets.QDialogButtonBox.Ok
+            | QtWidgets.QDialogButtonBox.Apply,
+            self,
+        )
+        self.button_box.clicked.connect(self.buttonClicked)
+
+        self.button_apply_all = None
+        if apply_all:
+            self.button_apply_all = self.button_box.addButton(
+                "Apply All", QtWidgets.QDialogButtonBox.ApplyRole
+            )
+            # self.button_apply_all.setIcon
+            self.button_apply_all.clicked.connect(self.applyAll)
 
         layout = QtWidgets.QVBoxLayout()
         layout.addLayout(self.layout_main)
-        layout.addLayout(self.layout_buttons)
+        layout.addWidget(self.button_box)
+
         self.setLayout(layout)
 
-    def sizeHint(self) -> QtCore.QSize:
-        return QtCore.QSize(800, 600)
+    def buttonClicked(self, button: QtWidgets.QAbstractButton) -> None:
+        sb = self.button_box.standardButton(button)
 
-    def isComplete(self) -> bool:
-        return True
+        if sb == QtWidgets.QDialogButtonBox.Apply:
+            self.apply()
+            self.applyPressed.emit()
+        elif sb == QtWidgets.QDialogButtonBox.Ok:
+            self.apply()
+            self.applyPressed.emit()
+            self.accept()
+        elif sb == QtWidgets.QDialogButtonBox.Cancel:
+            self.reject()
+
+    def accept(self) -> None:
+        self.restoreWidget()
+
+    def apply(self) -> None:
+        pass
+
+    def applyAll(self) -> None:
+        pass
 
     @QtCore.Slot()
     def completeChanged(self) -> None:
-        pass
+        enabled = self.isComplete()
+        self.button_box.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(enabled)
+        self.button_box.button(QtWidgets.QDialogButtonBox.Apply).setEnabled(enabled)
+        if self.button_apply_all is not None:
+            self.button_apply_all.setEnabled(enabled)
+
+    def isComplete(self) -> bool:
+        return True  # pragma: no cover
+
+    def reject(self) -> None:
+        self.restoreWidget()
+
+    def restoreWidget(self) -> None:
+        self.view.insertTab(self.index, self.widget.laser.name, self.widget)
+        self.view.removeTab(self.index)
+
+    def sizeHint(self) -> QtCore.QSize:
+        return QtCore.QSize(800, 600)
 
     def transform(self, **kwargs) -> None:
         if hasattr(self.widget, "transform"):
