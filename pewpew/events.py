6c154c8e69b2dec6f26fe224149b8aa36adf0d29
djdt <10266332+djdt@users.noreply.github.com>
2020-09-03 20:25:40 +1000

    drag and drop event filter

--- pewpew/events.py
+++ pewpew/events.py
@@ -7,3 +7,20 @@ class MousePressRedirectFilter(QtCore.QObject):
             self.parent().mousePressEvent(event)
             return False
         return bool(super().eventFilter(obj, event))
+
+
+class DragDropRedirectFilter(QtCore.QObject):
+    def eventFilter(self, obj: QtCore.QObject, event: QtCore.QEvent) -> bool:
+        if event.type() == QtCore.QEvent.DragEnter:
+            self.parent().dragEnterEvent(event)
+            return False
+        elif event.type() == QtCore.QEvent.DragLeave:
+            self.parent().dragLeaveEvent(event)
+            return False
+        elif event.type() == QtCore.QEvent.DragMove:
+            self.parent().dragMoveEvent(event)
+            return False
+        elif event.type() == QtCore.QEvent.Drop:
+            self.parent().dropEvent(event)
+            return False
+        return bool(super().eventFilter(obj, event))
