ac397006a9544a9aae4ca7de102e4309fbb67b8f
djdt <10266332+djdt@users.noreply.github.com>
2020-11-17 16:04:02 +1100

    stop propagation of redirected drop events

--- pewpew/events.py
+++ pewpew/events.py
@@ -13,14 +13,14 @@ class DragDropRedirectFilter(QtCore.QObject):
     def eventFilter(self, obj: QtCore.QObject, event: QtCore.QEvent) -> bool:
         if event.type() == QtCore.QEvent.DragEnter:
             self.parent().dragEnterEvent(event)
-            return False
+            return True
         elif event.type() == QtCore.QEvent.DragLeave:
             self.parent().dragLeaveEvent(event)
-            return False
+            return True
         elif event.type() == QtCore.QEvent.DragMove:
             self.parent().dragMoveEvent(event)
-            return False
+            return True
         elif event.type() == QtCore.QEvent.Drop:
             self.parent().dropEvent(event)
-            return False
+            return True
         return bool(super().eventFilter(obj, event))
