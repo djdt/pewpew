7296a09b310d0802b45ea954a15aebf4049f4726
djdt <10266332+djdt@users.noreply.github.com>
2020-11-23 22:27:22 +1100

    finish import tutorial

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'pewpew'
-copyright = '2020, djdt'
-author = 'djdt'
+project = "pewpew"
+copyright = "2020, djdt"
+author = "djdt"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,19 +28,19 @@ author = 'djdt'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'matplotlib.sphinxext.plot_directive',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_autodoc_typehints',
+    "matplotlib.sphinxext.plot_directive",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -48,9 +48,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+
+rst_epilog = """.. |pewpew| replace:: pew²"""
--- docs/tutorials/import.rst
+++ docs/tutorials/import.rst
@@ -2,7 +2,7 @@
 Importing Data
 ==============
 
-Supported formats.
+.. table:: Vendor formats supported by |pewpew|.
 
 +-----------+-----------+-------------+--------------+
 |Vendor     |Software   |Format       |Tested        |
@@ -14,7 +14,20 @@ Supported formats.
 |PerkinElmer|           |directory    |              |
 +-----------+-----------+-------------+--------------+
 
-For the majority of users importing data consists of dragging-and-dropping of files into pewpew.
+For the majority of users importing data consists of dragging-and-dropping of files into |pewpew|.
 An `Import Wizard` exists for cases of incorrectly formatted data or if you required finer control over an import.
 
+Import Wizard
+-------------
+
 * **File -> Import -> Import Wizard**
+
+Importing data using the `Import Wizard` begins by selecting the format to be used.
+The path of the file or directory can be chosen on the next page using either the file dialog `Open File`
+or by drag-and-drop of the file into the import wizard.
+
+Once the file has been selected format options will be automatically filled with sensible defaults.
+These can be changed if required, for example to extract the analog channel from a Thermo iCAP CSV.
+
+The final page of the wizard allows the user to fill in any incorrect laser parameters and to rename or delete
+imported isotopes. Clicking finish will import the data into |pewpew|.
