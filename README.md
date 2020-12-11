5c2561aa9bc481e266cb266ccaf5ab242649aaed
djdt <10266332+djdt@users.noreply.github.com>
2020-12-11 14:41:02 +1100

    add readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# pew²
+
+Pew² is a GUI for importing and processing line-by-line collected LA-ICP-MS data
+using the python liibrary [pewlib](https://github.com/djdt/pewlib).
+
+## Installation
+
+Windows executables are available for each release.
+
+To install via pip first clone the repository then install as a local package.
+
+```bash
+git clone https://github.com/djdt/pewpew
+cd pewpew
+pip install -e .
+```
--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as fp:
+    long_description = fp.read()
+
 setup(
     name="pewpew",
+    version="1.1.0",
     description="GUI for visualisation and manipulation of LA-ICP-MS data.",
-    url="https://github.com/djdt/pewpew",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="T. Lockwood",
     author_email="thomas.lockwood@uts.edu.au",
-    version="1.1.0",
+    url="https://github.com/djdt/pewpew",
+    project_urls={
+        # "Documentation": "https://djdt.github.io/pewlib",
+        "Source": "https://gtihub.com/djdt/pewpew",
+    },
     packages=find_packages(include=["pewpew", "pewpew.*"]),
     install_requires=[
         "numpy",
         "matplotlib>=3.0.0",
-        "pewlib>=0.6.2",
+        "pewlib>=0.6.3",
         "PySide2",
     ],
     entry_points={"console_scripts": ["pewpew=pewpew.__main__:main"]},
