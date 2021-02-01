0cc0b439658a6c779bb111fc122637389c9f8f86
djdt <10266332+djdt@users.noreply.github.com>
2021-02-01 17:22:35 +1100

    remove print

--- pewpew/models.py
+++ pewpew/models.py
@@ -98,5 +98,4 @@ class CalibrationPointsTableModel(NumpyArrayTableModel):
         else:
             self.calibration.points = self.array[:, :2]
 
-        print(self.array)
         self.calibration.update_linreg()
