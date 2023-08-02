74adcc06863406f2dc75e447f9995339e123cba5
djdt <10266332+djdt@users.noreply.github.com>
2023-08-02 14:42:54 +1000

    special case with n=2

--- pewpew/graphics/util.py
+++ pewpew/graphics/util.py
@@ -31,9 +31,9 @@ def closest_nice_value(
     nice = np.divide(values, 10**e, where=values != 0.0)
 
     if mode == "upper":
-        idx = np.searchsorted(allowed, np.abs(nice), side="right") - 1
-    elif mode == "lower":
         idx = np.searchsorted(allowed, np.abs(nice), side="left")
+    elif mode == "lower":
+        idx = np.searchsorted(allowed, np.abs(nice), side="right") - 1
     else:
         raise ValueError("'mode' must be one of 'upper', 'lower'")
     idx = np.clip(idx, 0, allowed.size - 1)
@@ -42,10 +42,13 @@ def closest_nice_value(
 
 def nice_values(vmin: float, vmax: float, n: int = 6) -> np.ndarray:
     lower = closest_nice_value(vmin, mode="upper")
+    upper = closest_nice_value(vmax, mode="lower")
+    if n == 2:
+        return np.array([lower, upper])
     interval = closest_nice_value(
-        (vmax - lower) / n,
+        (upper - lower) / n,
         allowed=np.array([0.0, 1.0, 2.0, 2.5, 5.0, 7.5]),
-        mode="lower",
+        mode="upper",
     )
     return np.arange(lower, vmax * 1.001, interval)
 
@@ -75,8 +78,8 @@ def path_for_colorbar_labels(
 
     check_width = fm.xHeight() / 4.0
 
-    for n in np.arange(7, 1, -1):
-        max_text_width = shortest_label(fm, 8.88e88)[1]
+    for n in np.arange(6, 1, -1):
+        max_text_width = shortest_label(fm, 8.8e88)[1]
         if max_text_width * n < width:
             break
 
