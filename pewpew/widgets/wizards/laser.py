44260c2c64dfbcd620262f25a97bf7320ee6c840
djdt <10266332+djdt@users.noreply.github.com>
2024-05-10 10:40:48 +1000

    tree view for grouping lasers

--- pewpew/widgets/wizards/laser.py
+++ pewpew/widgets/wizards/laser.py
@@ -1,23 +1,13 @@
 import logging
-import time
-from importlib.metadata import version
 from pathlib import Path
 
 import numpy as np
-import numpy.lib.recfunctions as rfn
 from pewlib.config import SpotConfig
 from pewlib.io.laser import read_nwi_laser_log
 from pewlib.laser import Laser
-from pewlib.process import peakfinding
-from pewlib.process.calc import view_as_blocks
 from PySide6 import QtCore, QtGui, QtWidgets
 
-from pewpew.charts.colors import sequential
-from pewpew.charts.signal import SignalChart
-from pewpew.graphics.lasergraphicsview import LaserGraphicsView
 from pewpew.graphics.options import GraphicsOptions
-from pewpew.validators import DecimalValidator, DecimalValidatorNoZero, OddIntValidator
-from pewpew.widgets.dialogs import NameEditDialog
 from pewpew.widgets.wizards.import_ import FormatPage
 from pewpew.widgets.wizards.options import PathAndOptionsPage, PathSelectWidget
 
@@ -71,20 +61,24 @@ class LaserLogImportPage(QtWidgets.QWizardPage):
     log_prop = QtCore.Property("QVariant", getLog, setLog, notify=logChanged)
 
 
-# class LaserGroupTreeItem(QtWidgets.QTreeWidgetItem):
-#     def __init__(self, seq: int, comment: str, num: int) -> None:
-#         super().__init__(QtWidgets.QTreeWidgetItem.ItemType.UserType)
-#
-#         self.seq = seq
-#
-#         label = QtWidgets.QLabel(f"Sequence {seq} :: {comment}")
-#         num_label = QtWidgets.QLabel(f"Num lines = {num}")
-#
-#         layout = QtWidgets.QVBoxLayout()
-#         layout.addWidget(label)
-#         layout.addWidget(num_label)
-#         self.setLayout(layout)
-#
+class LaserGroupListItem(QtWidgets.QListWidgetItem):
+    def __init__(self, seq: int, comment: str, num: int) -> None:
+        super().__init__()
+
+        self.seq = seq
+
+        self.lasers = QtWidgets.QListWidget()
+
+        label = QtWidgets.QLabel(f"Sequence {seq} :: {comment}\nlines = {num}")
+
+        layout = QtWidgets.QHBoxLayout()
+
+        gbox = QtWidgets.QGroupBox("Lasers")
+        gbox.setLayout(QtWidgets.QVBoxLayout())
+        gbox.layout().addWidget(self.lasers)
+        layout.addWidget(label, 0)
+        layout.addWidget(gbox, 1)
+        self.setLayout(layout)
 
 
 class LaserGroupsImportPage(QtWidgets.QWizardPage):
@@ -94,10 +88,16 @@ class LaserGroupsImportPage(QtWidgets.QWizardPage):
     def __init__(self, parent: QtWidgets.QWidget | None = None):
         super().__init__(parent)
 
-        self._datas = []
-        self._infos = []
+        self._datas: list[np.ndarray] = []
+        self._infos: list[dict] = []
 
         self.group_tree = QtWidgets.QTreeWidget()
+        self.group_tree.setColumnCount(4)
+        self.group_tree.setHeaderLabels(["Sequence", "Name", "No. Lines", "Lasers"])
+        self.group_tree.setDragEnabled(True)
+        self.group_tree.setDragDropMode(
+            QtWidgets.QAbstractItemView.DragDropMode.InternalMove
+        )
 
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(self.group_tree)
@@ -113,16 +113,42 @@ class LaserGroupsImportPage(QtWidgets.QWizardPage):
         )
         comments = log_data["comment"][seq_idx]
         num_lines = [
-            np.count_nonzero(log_data[log_data["sequence"] == i]["state"] == "On")
-            for i in seq_idx
+            np.count_nonzero(
+                log_data[(log_data["sequence"] == i) & (log_data["state"] == "On")]
+            )
+            for i in sequences
         ]
 
-        for seq, comment, num in zip(sequences, comments, num_lines):
-            print(seq, comment, num)
-            item = QtWidgets.QTreeWidgetItem(f"Sequence {seq} :: {comment} :: {num} lines.")
+        self.group_tree.clear()
+        for row, (seq, comment, num) in enumerate(zip(sequences, comments, num_lines)):
+            item = QtWidgets.QTreeWidgetItem()
+            item.setData(0, 0, seq)
+            item.setText(0, str(seq))
+            item.setText(1, comment)
+            item.setText(2, str(num))
+            item.setFlags(
+                QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemFlag.ItemIsDropEnabled
+            )
             self.group_tree.addTopLevelItem(item)
 
-        # self.
+        infos = self.field("laserinfo")
+        for i, info in enumerate(infos):
+            item = self.group_tree.topLevelItem(i % self.group_tree.topLevelItemCount())
+            child = QtWidgets.QTreeWidgetItem()
+            child.setData(0, 0, i)
+            child.setText(0, "---")
+            child.setText(1, "---")
+            child.setText(2, "---")
+            child.setIcon(3, QtGui.QIcon.fromTheme("drag-handle-symbolic"))
+            child.setText(3, info["Name"])
+            child.setFlags(
+                QtCore.Qt.ItemIsSelectable
+                | QtCore.Qt.ItemIsEnabled
+                | QtCore.Qt.ItemFlag.ItemIsDragEnabled
+            )
+            item.addChild(child)
+
+        self.group_tree.expandAll()
 
     def getData(self) -> list[np.ndarray]:
         if len(self._datas) == 0:
@@ -174,6 +200,10 @@ class LaserLogImportWizard(QtWidgets.QWizard):
         if laser_paths is None:
             laser_paths = []
         paths = [Path(x) for x in laser_paths]
+
+        # @nocommit
+        path = Path("/home/tom/Downloads/LaserLog_24-05-02_13-49-34.csv")
+        paths = [Path("/home/tom/Downloads/lasso.b/")]
 
         self.setPage(self.page_laser, LaserLogImportPage(path))
 
