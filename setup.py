41d7edaabc215ac009af6f32151c647d73b29a95
djdt <10266332+djdt@users.noreply.github.com>
2024-10-17 14:44:23 +1100

    use numpy 2 c-api

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ polyext = Extension(
     "pewpew.lib.polyext",
     sources=["src/polyextmodule.c"],
     include_dirs=[numpy.get_include()],
-    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_2_0_API_VERSION")],
 )
 
 setup(
