4eca828f038ec09207bd59d763ef7988cb0d21f8
djdt <10266332+djdt@users.noreply.github.com>
2026-05-07 15:09:53 +0200

    add a nu vitesse wiz

--- pewpew/widgets/wizards/__init__.py
+++ pewpew/widgets/wizards/__init__.py
@@ -3,3 +3,4 @@ from .spot import SpotImportWizard
 from .srr import SRRImportWizard
 from .laser import LaserLogImportWizard
 from .imzml import ImzMLImportWizard
+from .nu import NuVitesseImportWizard
